49527a83b0ca465fc8cf1cf5682be0bac0825a49
Vishal Kumar Gupta <vishaaal05@gmail.com>
2024-08-30 15:24:25 +0530

    config prisma

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,7 @@
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
+
 export default function Home() {
   return (
     <div className="text-slate-700 bg-black text-2xl">
